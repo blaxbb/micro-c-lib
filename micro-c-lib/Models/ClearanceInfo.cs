1b87b2a6792463cb0e358527dfe70b77b14142cf
Brian Barrett <blaxbb@gmail.com>
2022-04-26 20:05:04 -0400

    Add empty Location property to clearanceinfo

--- micro-c-lib/Models/ClearanceInfo.cs
+++ micro-c-lib/Models/ClearanceInfo.cs
@@ -10,9 +10,11 @@ namespace micro_c_lib.Models
         private string state;
         private float price;
         private string id;
+        private string location;
 
         public string Id { get => id; set => SetProperty(ref id, value); }
         public string State { get => state; set => SetProperty(ref state, value); }
         public float Price { get => price; set => SetProperty(ref price, value); }
+        public string Location { get => location; set => SetProperty(ref location, value); }
     }
 }
